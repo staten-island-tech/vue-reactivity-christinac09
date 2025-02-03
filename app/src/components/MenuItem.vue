cbb06b514b9b71fb25ebfcc59b40156093f982c7
christina <158211764+christinac09@users.noreply.github.com>
2025-02-03 14:00:03 -0500

    idk what im doing

--- app/src/components/MenuItem.vue
+++ app/src/components/MenuItem.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="card bg-base-100 w-96 shadow-xl">
     <figure>
-      <img
-        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-        alt="Shoes"
-      />
+      <img :src="item.image" alt="Shoes" />
     </figure>
     <div class="card-body">
       <h2 class="card-title">{{ item.name }}</h2>
       <h3>{{ item.price }}</h3>
       <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+        <button class="btn btn-primary">Add</button>
       </div>
     </div>
   </div>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <template>
   <main>
-    <MenuItems v-for="item in items" :key="item.name" />
+    <MenuItems v-for="item in items" :key="item.name" :item="item" />
   </main>
 </template>
 
 <script setup>
 import MenuItems from '../components/MenuItem.vue'
-const items = [{ name: 'test', price: 5 }]
+const items = [
+  { name: 'something', price: 5, image: 'placeholder.jpg' },
+  { name: 'another thing', price: 10, image: 'placeholder.jpg' },
+]
 </script>
 
 <style scoped></style>
