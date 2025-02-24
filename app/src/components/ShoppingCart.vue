b78faf8b5a95f0827233f00a99831a54656881c3
christina <158211764+christinac09@users.noreply.github.com>
2025-02-24 12:17:51 -0500

    help me

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-wrap flex-row bg-[#d5bda5be] w-[35%] absolute right-0 top-[10rem] overflow-y-auto max-h-full scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200"
+    class="flex flex-wrap flex-row bg-[#d5bda5be] w-[35%] min-h-[80vh] absolute right-0 top-[10rem] overflow-y-auto max-h-full scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200"
   >
     <h2 class="text-3xl text-center font-bold w-full m-2">Shopping Cart</h2>
     <h3 class="text-xl text-center w-full">Total Cost: ${{ cart.totalCost }}</h3>
