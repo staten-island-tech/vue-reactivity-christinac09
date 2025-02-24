6ba4d71474a4a15d67207674628fa074040c051a
christina <158211764+christinac09@users.noreply.github.com>
2025-02-24 12:14:18 -0500

    DJFLSJFLSKJF

--- app/src/components/CardContainer.vue
+++ app/src/components/CardContainer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex flex-wrap flex-row w-[65%] absolute left-0 top-[10rem] overflow-y-auto max-h-full scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200" id="card-container">
+  <div
+    class="flex flex-wrap flex-row w-[65%] absolute left-0 top-[10rem] overflow-y-auto max-h-full scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200"
+    id="card-container"
+  >
     <ToppingCards v-for="item in items" :key="item.name" :item="item" />
     <!-- :item is a prop and it expects a child component -->
   </div>
