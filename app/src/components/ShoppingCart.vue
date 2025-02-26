e27ab521fe4f30affa31225c07d530ff6e87bb68
christina <christinac272@nycstudents.net>
2025-02-26 01:17:36 -0500

    Update ShoppingCart.vue

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-wrap flex-row bg-[#d5bda5be] w-[35%] min-h-[80vh] absolute right-0 top-[10rem] overflow-y-auto max-h-full scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200"
+    class="flex flex-wrap flex-row bg-[#d5bda5be] w-[35%] min-h-[80vh] absolute right-0 top-[10rem] overflow-y-auto max-h-[81vh] scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200"
   >
     <h2 class="text-3xl text-center font-bold w-full m-2 mt-4">Your Bowl</h2>
     <h3 class="text-xl text-center text-[#53443d] w-full">Total Cost: ${{ cart.totalCost.toFixed(2) }}</h3>
